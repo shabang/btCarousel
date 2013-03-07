3a1a0b27f7a41d2ad0fac0b0467d59e0bffcb086
Daniel F. <skz@shebang-labs.fr>
2013-03-07 10:38:05 +0100

    Create project

---
+++ btCaroussel/btCaroussel_migx.md
@@ -0,0 +1,47 @@
+# **btCarousel** : Utiliser MIGX avec Bootstrap Carousel 
+
+
+## Construction du migx
+
+Nous aurons besoin de 3 TV (2 type texte et 1 type image). Et d'un 4e TV, qui sera le migX lui-même.
+
+Dans Form tab, j'ai mis ce JSON :
+
+    [
+        {"caption":"Slider", "fields": [
+            {"field":"slidtitle","caption":"Titre","inputTV":"slid_title"},
+            {"field":"slidDesc","caption":"Description","inputTV":"slid_desc"}, 
+            {"field":"slidImg","caption":"Image","inputTV":"slid_img"}
+        ]}
+    ] 
+
+Et dans Grid Columns :
+
+    [
+        {"header": "Titre de l'item", "width": "100", "sortable": "false", "dataIndex": "slidtitle"},
+        {"header": "Description de l'item", "width": "200", "sortable": "false", "dataIndex": "slidDesc"}, 
+        {"header": "Image de l'item", "width": "100", "sortable": "false", "dataIndex": "slidImg", "renderer": "this.renderImage"}
+    ]    
+
+
+## Les chunks
+
+Le snippet nécessite de 2 chunks :
+
+- Un chunk pour l'affichage des items
+- Un chunk pour l'affichage du conteneur
+
+## Le snippet
+
+
+Le snippet nécessite 3 paramètres :
+
+- **tv** : le tv du migx
+- **itemTpl** : le chunk des items 
+- **containerTpl** : le container
+
+L'appel du snippet se fait comme ceci :
+
+    [[btSlider?&tv=`homeSlider`&itemTpl=`slidItem`&containerTpl=`slidContainer`]]
+
+
---
+++ btCaroussel/snippet/btCaroussel.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @purpose   Affichage d'un migx avec Bootstrap Caroussel
+ *===============================================================================
+ * @version   0.1
+ * @date      06/02/2013
+ * @author    d.feldle@ackwa.fr
+ * @see       http://www.ackwa.fr
+ * @require   migx
+ * @copyright (c) 2010 - Ackwa
+ *===============================================================================
+ * @history   0.1 - 06/02/13 - Création
+ *===============================================================================
+ * @usage     [[btSlider?&tv=`migxTV`&itemTpl=`contentChunk`&containerTpl=`containerChunk`]]
+ *===============================================================================
+ */
+
+/*
+ * On récupère nos paramètres
+ */
+$tv           = $modx->getOption('tv',           $scriptProperties, False);
+$itemTpl      = $modx->getOption('itemTpl',      $scriptProperties, False);
+$containerTpl = $modx->getOption('containerTpl', $scriptProperties, False);
+$identifiant  = $modx->getOption('identifiant',  $scriptProperties, "main-banner");
+
+if ($tv && $itemTpl && $containerTpl){
+    $tabParam['identifiant'] = $identifiant;
+    /*
+     * On récupère le nombre de ligne du migx
+     */
+    $oRes  = $modx->getObject('modResource', $modx->resource->get('id'));
+    $sData = $oRes->getTVValue($tv);
+    $lData = json_decode($sData);
+    $MIGX_id = 0;
+    foreach($lData as $oData) {
+        $MIGX_id = max($MIGX_id, $oData->MIGX_id);
+    }
+    /*
+     * Construction des indicateurs
+     */
+    $tabParam['indicator'] ='';
+    for ($i=0; $i < $MIGX_id ; $i++) { 
+        $tabParam['indicator'] .= '<li data-target="'.$identifiant.'" data-slide-to="'.$i.'" class="';
+        $tabParam['indicator'] .= (($i==0)?'active':'');
+        $tabParam['indicator'] .= '"></li>';
+    }
+    
+    /*
+     * Mise en place des item
+     */
+    $getImageListParams = array(
+        'tvname'    => $tv,
+        'tpl'       => $itemTpl 
+    );  
+    $tabParam['item'] = $modx->runSnippet('getImageList', $getImageListParams); 
+
+    return $modx->getChunk($containerTpl, $tabParam);  
+}
+else{
+    $modx->log(xPDO::LOG_LEVEL_ERROR, "Paramètre(s) manquant(s)");
+    return false;
+}
